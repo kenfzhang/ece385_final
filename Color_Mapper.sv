3f5ebbe2413af844b4de35d1fb1e3f77948ffae4
Scott Xiong <scott@Scotts-MacBook-Pro.local>
2016-12-03 19:23:01 -0600

    reverted back to old font table

--- Color_Mapper.sv
+++ Color_Mapper.sv
@@ -26,23 +26,15 @@ module  color_mapper ( input        [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
 
     // parameters for the platforms
     logic platform_on;
-    logic [10:0] platformL_X = 240;
-    logic [10:0] platformM1_X = platformL_X + 16;
-    logic [10:0] platformM2_X = platformM1_X + 16;
-    logic [10:0] platformM3_X = platformM2_X + 16;
-    logic [10:0] platformR_X = platformM3_X + 16;
+    logic [10:0] platform_X = 240;
 	logic [10:0] platform_Y = 470;
-	logic [10:0] platform_size_X = 16; 
-	logic [10:0] platform_size_Y = 4;
+	logic [10:0] platform_size_X = 8; 
+	logic [10:0] platform_size_Y = 16;
 
     // font rom stuff, only used for doodle guy
     logic [10:0] sprite_addr;
     logic [7:0] sprite_data;
     font_rom (.addr(sprite_addr), .data(sprite_data));
-    // font rom for platforms
-    logic [10:0] platform_addr;
-    logic [15:0] platform_data;
-    platform_sprite (.addr(platform_addr), .data(platform_data));
 
     always_comb
     begin:Ball_on_proc
@@ -57,40 +49,12 @@ module  color_mapper ( input        [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
 		 end
 
          // drawing the platform sprite
-		 else if(DrawX >= platformL_X && DrawX < platformL_X + platform_size_X &&
+		 else if(DrawX >= platform_X && DrawX < platform_X + platform_size_X &&
 			      DrawY >= platform_Y && DrawY < platform_Y + platform_size_Y)
 		 begin
 			ball_on = 1'b0;
 			platform_on = 1'b1;
-            platform_addr = (DrawY - platform_Y + 4*'h00);
-	    end
-        else if(DrawX >= platformM1_X && DrawX < platformM1_X + platform_size_X &&
-			      DrawY >= platform_Y && DrawY < platform_Y + platform_size_Y)
-		 begin
-			ball_on = 1'b0;
-			platform_on = 1'b1;
-            platform_addr = (DrawY - platform_Y + 4*'h01);
-	    end
-        else if(DrawX >= platformM2_X && DrawX < platformM2_X + platform_size_X &&
-			      DrawY >= platform_Y && DrawY < platform_Y + platform_size_Y)
-		 begin
-			ball_on = 1'b0;
-			platform_on = 1'b1;
-            platform_addr = (DrawY - platform_Y + 4*'h01);
-	    end
-        else if(DrawX >= platformM3_X && DrawX < platformM3_X + platform_size_X &&
-			      DrawY >= platform_Y && DrawY < platform_Y + platform_size_Y)
-		 begin
-			ball_on = 1'b0;
-			platform_on = 1'b1;
-            platform_addr = (DrawY - platform_Y + 4*'h01);
-	    end
-        else if(DrawX >= platformR_X && DrawX < platformR_X + platform_size_X &&
-			      DrawY >= platform_Y && DrawY < platform_Y + platform_size_Y)
-		 begin
-			ball_on = 1'b0;
-			platform_on = 1'b1;
-            platform_addr = (DrawY - platform_Y + 4*'h02);
+            platform_addr = (DrawY - platform_Y + 16*'h00);
 	    end
 
         // else, draw nothing
@@ -106,7 +70,7 @@ module  color_mapper ( input        [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
     always_comb
     begin:RGB_Display
 
-        // display the bouncing ball: Yellow
+        // display the bouncing ball
         if ((ball_on == 1'b1) && (sprite_data[DrawX - ball_X] == 1'b1)) 
         begin 
             Red = 8'h00;
@@ -114,32 +78,8 @@ module  color_mapper ( input        [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
             Blue = 8'hff;
         end
 
-        // display the shape: Teal
-        else if ((platform_on == 1'b1) && (platform_data[DrawX - platformL_X] == 1'b1))
-        begin
-            Red = 8'hff;
-            Green = 8'hff;
-            Blue = 8'h00;
-        end
-        else if ((platform_on == 1'b1) && (platform_data[DrawX - platformM1_X] == 1'b1))
-        begin
-            Red = 8'hff;
-            Green = 8'hff;
-            Blue = 8'h00;
-        end
-        else if ((platform_on == 1'b1) && (platform_data[DrawX - platformM2_X] == 1'b1))
-        begin
-            Red = 8'hff;
-            Green = 8'hff;
-            Blue = 8'h00;
-        end
-        else if ((platform_on == 1'b1) && (platform_data[DrawX - platformM3_X] == 1'b1))
-        begin
-            Red = 8'hff;
-            Green = 8'hff;
-            Blue = 8'h00;
-        end
-        else if ((platform_on == 1'b1) && (platform_data[DrawX - platformR_X] == 1'b1))
+        // display the platform
+        else if ((platform_on == 1'b1) && (platform_data[DrawX - platform_X] == 1'b1))
         begin
             Red = 8'hff;
             Green = 8'hff;
